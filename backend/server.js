2fd7c5445a6dc57b485093cb55d55cfe5a26a71b
ALREBDAWI <mahmoudrebdawi@gmail.com>
2024-08-10 15:40:28 +0200

    express server configuration + test

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const http = require('http');
-const server = http.createServer((req, res) => {
-    res.end('server response test')
-});
+
+const app = require('./app');
+app.set('port', process.env.PORT || 3000);
+
+const server = http.createServer(app);
 server.listen(process.env.PORT || 3000);
