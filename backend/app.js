2a24207bdcb512da7a86b19a942fa7e879e738e0
ALREBDAWI <mahmoudrebdawi@gmail.com>
2024-08-15 14:50:38 +0200

    mongoDB configuration+connection test is ok

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,33 @@
+
+
 const express = require('express');
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+console.log(process.env.DB_PASSWORD+' test');
+mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@cluster0.ogdde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+
+        .then(() => console.log('connected to mongoDB succesfully !'))
+        .catch(() => console.log('probleme with connection to mongoDB'));        
+
+
 const app = express();
-app.use((req, res) => {
-    res.json({ test: 'express server works!' })
+
+app.use((req, res, next) => {
+    res.status(200).json({connection: 'hello world'}) ;
+    next();
 });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+
+
+
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const http = require('http');
-
 const app = require('./app');
 app.set('port', process.env.PORT || 3000);
 
